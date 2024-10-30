2d7bc52a738b4fca01a24575c2af96ea221749c7
RafaelReisyzx <102748011+RafaelReisyzx@users.noreply.github.com>
2024-10-29 21:35:50 -0300

    Update system.h

--- src/system.h
+++ src/system.h
@@ -72,19 +72,18 @@ typedef struct {
     Instrucoes registradorInstrucao;
 } SYSTEM;
 
-#include "system.h"
-
 void fetch(SYSTEM *system);
 void decode(SYSTEM *system, int *operando1, int *operando2, int *operador, int *coreIDR, int *coreIDR1, int *coreIDR2, int *saida, int *r, int *r1, int *r2);
-void execute(SYSTEM *system, int operador, int operando1, int operando2, int r, int r1, int r2, int coreIDR, int coreIDR1, int coreIDR2) ;
+int execute(SYSTEM *system, int operador, int operando1, int operando2, int r, int r1, int r2, int coreIDR, int coreIDR1, int coreIDR2,int resultado) ;
 void memory_access(SYSTEM *system, int operador, int *saida, int *operando1, int coreIDR, int coreIDR1, int r, int r1) ;
+void write_back(SYSTEM *system,int operador,int coreIDR,int r,int resultado);
 void Pipeline(SYSTEM *system,int i);
 void inicializarSistema(SYSTEM *system);
 void exibirMemoriaSecundaria(MemoriaSecundaria *disco);
 void exibirMemoriaPrincipal(MemoriaPrincipal *memoria);
 void exibirRegistradores(Core *core);
 void exibirMenu(SYSTEM *system);
-void ULA(SYSTEM *system,int opcode, int reg1, int reg2,int r,int r1,int r2,int coreIDR,int coreIDR1,int coreIDR2);
+int ULA(SYSTEM *system,int opcode, int reg1, int reg2,int r,int r1,int r2,int coreIDR,int coreIDR1,int coreIDR2);
 void UC(SYSTEM *system, int *operando1, int *operando2, int *operador,int *coreIDR,int *coreIDR1,int *coreIDR2,int *saida,int *r,int *r1,int *r2);
 void escreverMemoriaPrincipal(MemoriaPrincipal *memoria);
 void escreverMemoriaSecundaria(MemoriaSecundaria *disco, int endereco, int valor);
