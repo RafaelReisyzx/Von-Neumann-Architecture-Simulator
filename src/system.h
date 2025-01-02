6183c04f1f60acfa6e39472ddb9ba0e8e139cd76
RafaelReisyzx <102748011+RafaelReisyzx@users.noreply.github.com>
2025-01-02 11:40:36 -0300

    Update system.h

--- src/system.h
+++ src/system.h
@@ -62,31 +62,35 @@ typedef struct {
     int registradores[NUM_REGISTRADORES]; 
 } Core;
 
-typedef struct {
-    Core cores[NUM_CORES];
-    int pc;           
-    Cache cache;                  
+typedef struct {              
     MemoriaPrincipal memoria;    
     MemoriaSecundaria disco;   
     Periferico periferico[NUM_PERIFERICOS]; 
-    Instrucoes registradorInstrucao;
 } SYSTEM;
 
-void fetch(SYSTEM *system);
-void decode(SYSTEM *system, int *operando1, int *operando2, int *operador, int *coreIDR, int *coreIDR1, int *coreIDR2, int *saida, int *r, int *r1, int *r2);
-int execute(SYSTEM *system, int operador, int operando1, int operando2, int r, int r1, int r2, int coreIDR, int coreIDR1, int coreIDR2,int resultado) ;
-void memory_access(SYSTEM *system, int operador, int *saida, int *operando1, int coreIDR, int coreIDR1, int r, int r1) ;
-void write_back(SYSTEM *system,int operador,int coreIDR,int r,int resultado);
-void Pipeline(SYSTEM *system,int i);
-void inicializarSistema(SYSTEM *system);
+typedef struct {
+    Core cores[NUM_CORES];        
+    int pc;                        
+    Cache cache;                  
+    Instrucoes registradorInstrucao;
+} CPU;
+
+
+void fetch(SYSTEM *system,CPU *cpu);
+void decode(CPU *cpu, int *operando1, int *operando2, int *operador, int *coreIDR, int *coreIDR1, int *coreIDR2, int *saida, int *r, int *r1, int *r2);
+int execute(CPU *cpu, int operador, int operando1, int operando2, int r, int r1, int r2, int coreIDR, int coreIDR1, int coreIDR2,int resultado) ;
+void memory_access(CPU *cpu,SYSTEM *system, int operador, int *saida, int *operando1, int coreIDR, int coreIDR1, int r, int r1) ;
+void write_back(CPU *cpu,int operador,int coreIDR,int r,int resultado);
+void Pipeline(CPU *cpu,SYSTEM *system,int i);
+void inicializarSistema(CPU *cpu,SYSTEM *system);
 void exibirMemoriaSecundaria(MemoriaSecundaria *disco);
 void exibirMemoriaPrincipal(MemoriaPrincipal *memoria);
 void exibirRegistradores(Core *core);
-void exibirMenu(SYSTEM *system);
-int ULA(SYSTEM *system,int opcode, int reg1, int reg2,int r,int r1,int r2,int coreIDR,int coreIDR1,int coreIDR2);
-void UC(SYSTEM *system, int *operando1, int *operando2, int *operador,int *coreIDR,int *coreIDR1,int *coreIDR2,int *saida,int *r,int *r1,int *r2);
+void exibirInformacoes(CPU *cpu,SYSTEM *system);
+int ULA(CPU *cpu,int opcode, int reg1, int reg2,int r,int r1,int r2,int coreIDR,int coreIDR1,int coreIDR2);
+void UC(CPU *cpu, int *operando1, int *operando2, int *operador,int *coreIDR,int *coreIDR1,int *coreIDR2,int *saida,int *r,int *r1,int *r2);
 void escreverMemoriaPrincipal(MemoriaPrincipal *memoria);
 void escreverMemoriaSecundaria(MemoriaSecundaria *disco, int endereco, int valor);
-void InstructionsMemory(SYSTEM *system,int code,int *saida, int *operando1,int coreIDR,int coreIDR1,int r,int r1);
+void InstructionsMemory(CPU *cpu,SYSTEM *system,int code,int *saida, int *operando1,int coreIDR,int coreIDR1,int r,int r1);
 
 #endif
