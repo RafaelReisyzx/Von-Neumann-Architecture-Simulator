93108961ec8d2d4716743c2891f0d0ac1f651a8e
RafaelReisyzx <102748011+RafaelReisyzx@users.noreply.github.com>
2025-01-02 21:40:28 -0300

    Update system.c

--- src/system.c
+++ src/system.c
@@ -1,49 +1,5 @@
 #include "system.h"
 
-void fetch(SYSTEM *system,CPU *cpu) {
-    cpu->registradorInstrucao = system->memoria.dados[cpu->pc].instrucao;
-}
-
-void decode(CPU *cpu, int *operando1, int *operando2, int *operador, int *coreIDR, int *coreIDR1, int *coreIDR2, int *saida, int *r, int *r1, int *r2) {
-    UC(cpu, operando1, operando2, operador, coreIDR, coreIDR1, coreIDR2, saida, r, r1, r2);
-}
-
-int execute(CPU *cpu, int operador, int operando1, int operando2, int r, int r1, int r2, int coreIDR, int coreIDR1, int coreIDR2,int resultado) {
-    resultado=ULA(cpu, operador, operando1, operando2, r, r1, r2, coreIDR, coreIDR1, coreIDR2);
-    return resultado;
-}
-
-void memory_access(CPU *cpu,SYSTEM *system, int operador, int *saida, int *operando1, int coreIDR, int coreIDR1, int r, int r1) {
-    if (operador == LOAD) {
-        *saida = system->memoria.dados[r].inteiro;
-        InstructionsMemory(cpu,system, LOAD, saida, operando1, coreIDR, coreIDR1, r, r1);
-    } else if (operador == STORE) {
-        *saida = system->memoria.dados[r1].inteiro;
-        InstructionsMemory(cpu,system, STORE, saida, operando1, coreIDR, coreIDR1, r, r1);
-    }
-}
-
-void write_back(CPU *cpu,int operador,int coreIDR,int r,int resultado){
- if (operador != LOAD&&operador != STORE&&operador != IF_EQUAL&&operador != IF_LESS&&operador != IF_LESS) {   
-      cpu->cores[coreIDR].registradores[r]=resultado;
-    }
-
-}
-
-void Pipeline(CPU *cpu,SYSTEM *system, int i) {
-    int j, r, r1, r2, operando1, operando2, operador, coreIDR, coreIDR1, coreIDR2, saida,resultado;
-    
-    for (j = 0; j < i; j++) {
-        fetch(system,cpu);
-        decode(cpu, &operando1, &operando2, &operador, &coreIDR, &coreIDR1, &coreIDR2, &saida, &r, &r1, &r2);
-       resultado= execute(cpu, operador, operando1, operando2, r, r1, r2, coreIDR, coreIDR1, coreIDR2,resultado);
-        memory_access(cpu,system, operador, &saida, &operando1, coreIDR, coreIDR1, r, r1);
-        write_back(cpu,operador,coreIDR,r,resultado);
-        cpu->pc++; 
-    }
-}
-
-
 void inicializarSistema(CPU *cpu,SYSTEM *system) {
 	int i,j;
     for (i = 0; i < NUM_CORES; i++) {
@@ -96,53 +52,6 @@ void inicializarSistema(CPU *cpu,SYSTEM *system) {
     system->periferico[3].disponivel = true;
 }
 
-void exibirRegistradores(Core *core) {
-	int i;
-    printf("Registradores:\n");
-    for (i = 0; i < NUM_REGISTRADORES; i++) {
-        printf("R[%d] = %d\n", i, core->registradores[i]);
-    }
-}
-
-void exibirMemoriaPrincipal(MemoriaPrincipal *memoria) {
-		int i;
-    printf("Memória Principal:\n");
-    for (i = 0; i < MEM_SIZE; i++) {
-        printf("Mem[%d] = %d\n", i, memoria->dados[i].inteiro);
-    }
-}
-
-void exibirMemoriaSecundaria(MemoriaSecundaria *disco) {
-		int i;
-    printf("Memória Secundária:\n");
-    for (i = 0; i < DISK_SIZE; i++) {
-        printf("Disk[%d] = %d\n", i, disco->dados[i].inteiro);
-    }
-}
-
-void escreverMemoriaPrincipal(MemoriaPrincipal *memoria) {
-    int endereco, valor;
-    printf("Digite o endereço de memória (0 a %d): ", MEM_SIZE - 1);
-    scanf("%d", &endereco);
-    if (endereco < 0 || endereco >= MEM_SIZE) {
-        printf("Endereço inválido. Deve estar entre 0 e %d.\n", MEM_SIZE - 1);
-        return;
-    }
-    printf("Digite o valor a ser escrito: ");
-    scanf("%d", &valor);
-    memoria->dados[endereco].inteiro = valor;
-    printf("Valor %d escrito na Mem[%d].\n", valor, endereco);
-}
-
-void escreverMemoriaSecundaria(MemoriaSecundaria *disco, int endereco, int valor) {
-    if (endereco >= 0 && endereco < DISK_SIZE) {
-        disco->dados[endereco].inteiro = valor;
-        printf("Escrevendo %d na Memória Secundária na posição %d\n", valor, endereco);
-    } else {
-        printf("Erro: Endereço %d fora dos limites da Memória Secundária\n", endereco);
-    }
-}
-
 void exibirInformacoes(CPU *cpu,SYSTEM *system) {
     int i;
  
@@ -157,106 +66,9 @@ void exibirInformacoes(CPU *cpu,SYSTEM *system) {
    
 }
 
-int ULA(CPU *cpu,int opcode, int reg1, int reg2,int r,int r1,int r2,int coreIDR,int coreIDR1,int coreIDR2) {
-    int resultado;
-    switch (opcode) {
-        case 0: // ADD
-            resultado = reg1 + reg2;
-            printf("ADD REG %d (Core:%d) [%d] = REG %d (Core:%d) [%d] + REG %d (Core:%d) [%d] \n",r,coreIDR, resultado, r1,coreIDR1, reg1,r2,coreIDR2, reg2);         
-           cpu->cores[coreIDR].registradores[r]=resultado;
-        
-            break;
-        case 1: // SUB
-            resultado = reg1 - reg2;
-                     printf("SUB REG %d (Core:%d) [%d] = REG %d (Core:%d) [%d] - REG %d (Core:%d) [%d] \n",r,coreIDR, resultado, r1,coreIDR1, reg1,r2,coreIDR2, reg2);
-            cpu->cores[coreIDR].registradores[r]=resultado;
-          
-            break;
-        case 2: // MUL
-            resultado = reg1 * reg2;
-            printf("MUL REG %d (Core:%d) [%d] = REG %d (Core:%d) [%d] * REG %d (Core:%d) [%d] \n",r,coreIDR, resultado, r1,coreIDR1, reg1,r2,coreIDR2, reg2);
-           cpu->cores[coreIDR].registradores[r]=resultado;
-           
-            break;
-        case 3: // DIV
-            if (reg2 != 0) {
-                resultado = reg1 / reg2;
-            printf("DIV REG %d (Core:%d) [%d] = REG %d (Core:%d) [%d] / REG %d (Core:%d) [%d] \n",r,coreIDR, resultado, r1,coreIDR1, reg1,r2,coreIDR2, reg2);
-           cpu->cores[coreIDR].registradores[r]=resultado;
-       
-            } else {
-                printf("Erro de divisão por zero\n");
-            }
-            break;
-        case 4: // IF_GREATER
-            if (reg1 > reg2) {
-                printf("IF_GREATER -REG %d (Core:%d) [%d] é maior que REG %d (Core:%d) [%d]\n",r1,coreIDR1, reg1,r2,coreIDR2, reg2);
-               
-            }else{
-              printf("IF_GREATER - REG %d (Core:%d) [%d] não é maior que REG %d (Core:%d) [%d]\n",r1,coreIDR1, reg1,r2,coreIDR2, reg2);  
-            }
-            break;
-        case 5: // IF_LESS
-            if (reg1 < reg2) {
-                printf("IF_LESS - REG %d (Core:%d) [%d] é menor que REG %d (Core:%d) [%d]\n",r1,coreIDR1, reg1,r2,coreIDR2, reg2);
-              
-            }else{
-                  printf("IF_LESS - REG %d (Core:%d) [%d] não é menor que REG %d (Core:%d) [%d]\n",r1,coreIDR1, reg1,r2,coreIDR2, reg2);
-            }
-            break;
-        case 6: // IF_EQUAL
-            if (reg1 == reg2) {
-                printf("IF_EQUAL - REG %d (Core:%d) [%d] é igual a REG %d (Core:%d) [%d]\n",r1,coreIDR1, reg1,r2,coreIDR2, reg2);
-              
-            }else{
-                 printf("IF_EQUAL - REG %d (Core:%d) [%d] não é igual a REG %d (Core:%d) [%d]\n",r1,coreIDR1, reg1,r2,coreIDR2, reg2);
-            }
-            break;
-        default:
-       if (opcode != 7 && opcode != 8) {
-    printf("Opcode desconhecido: %d\n", opcode);
-}
-
-            break;
-    }
-      return resultado;
-}
-
-void UC(CPU *cpu, int *operando1, int *operando2, int *operador,int *coreIDR,int *coreIDR1,int *coreIDR2,int *saida,int *r,int *r1,int *r2) {
-    if (cpu->registradorInstrucao.Opcode == 0) {
-        *operador = ADD;
-    } else if (cpu->registradorInstrucao.Opcode == 1) {
-        *operador = SUB;
-    } else if (cpu->registradorInstrucao.Opcode == 2) {
-        *operador = MUL;
-    } else if (cpu->registradorInstrucao.Opcode == 3) {
-        *operador = DIV;
-    } else if (cpu->registradorInstrucao.Opcode == 4) {
-        *operador = IF_GREATER;
-    } else if (cpu->registradorInstrucao.Opcode == 5) {
-        *operador = IF_LESS;
-    } else if (cpu->registradorInstrucao.Opcode == 6) {
-        *operador = IF_EQUAL; 
-    } else if (cpu->registradorInstrucao.Opcode == 7) {
-        *operador = LOAD;
-    } else if (cpu->registradorInstrucao.Opcode == 8) {
-        *operador = STORE;
-    }
-    *coreIDR = cpu->registradorInstrucao.coreIDR;
-    *coreIDR1 = cpu->registradorInstrucao.coreIDR1;
-    *coreIDR2 = cpu->registradorInstrucao.coreIDR2;
-    *operando1 = cpu->cores[*coreIDR1].registradores[cpu->registradorInstrucao.Register1];
-    *operando2 = cpu->cores[*coreIDR2].registradores[cpu->registradorInstrucao.Register2];
-    *saida = cpu->cores[*coreIDR].registradores[cpu->registradorInstrucao.Register];
-    *r = cpu->registradorInstrucao.Register;
-    *r1 = cpu->registradorInstrucao.Register1;
-    *r2 =cpu->registradorInstrucao.Register2;
- 
-
-}
- 
 void InstructionsMemory(CPU *cpu,SYSTEM *system,int code,int *saida, int *operando1,int coreIDR,int coreIDR1,int r,int r1){
-
+int STORE=8;
+int LOAD=7;
  if (code == LOAD) {
      
         cpu->cores[coreIDR].registradores[r] = *saida;   
@@ -268,6 +80,4 @@ void InstructionsMemory(CPU *cpu,SYSTEM *system,int code,int *saida, int *operan
         printf("STORE MEM %d [%d] = REG %d (Core:%d) [%d] \n",r, *saida,r1,coreIDR, *operando1);
        *saida=*operando1;
     }
-
 }
-
