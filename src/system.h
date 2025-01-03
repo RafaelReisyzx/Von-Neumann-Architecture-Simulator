ff92d6c230178f2fb2a148945b91ba5acfb6faec
RafaelReisyzx <102748011+RafaelReisyzx@users.noreply.github.com>
2025-01-03 11:18:46 -0300

    Update system.h

--- src/system.h
+++ src/system.h
@@ -6,11 +6,14 @@
 #include <stdbool.h>
 #include <time.h>
 #include <string.h>
+#include <ctype.h>
 
-#include "memoria.h" 
+#include "memoria.h"
 #include "cpu.h"
 
 #define NUM_PERIFERICOS 4
+#define MAX_LINHA 100
+
 
 typedef struct {
     char* nome;
@@ -26,5 +29,8 @@ typedef struct {
 void inicializarSistema(CPU *cpu, SYSTEM *system);
 void exibirInformacoes(CPU *cpu, SYSTEM *system);
 void InstructionsMemory(CPU *cpu, SYSTEM *system, int code, int *saida, int *operando1, int coreIDR, int coreIDR1, int r, int r1);
+int opcodeFromString(const char* instrucao);
+void carregarInstrucoesDeArquivo(const char* caminho, SYSTEM* system, int* contador);
+void carregarValoresIniciais(const char *caminho, CPU *cpu, SYSTEM *system);
 
 #endif
